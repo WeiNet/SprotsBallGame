9485dccdfcd7623343d5443e97a3977af19fa870
Crystal <1434426976@qq.com>
2016-03-02 10:09:41 +0800

    Initial Commit

---
+++ SportsBall/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SportsBall
+//
+//  Created by abel jing on 16/3/2.
+//  Copyright © 2016年 abel jing. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
