c2e91c17e0bf592b328384b5d34ee634387efd0e
Brook He <heyupei@brookmacbook-air.local>
2016-04-28 10:05:08 +0800

    是是是

---
+++ SportsBall/RollView.swift
@@ -0,0 +1,15 @@
+//
+//  RollView.swift
+//  SportsBall
+//
+//  Created by Brook on 16/4/28.
+//  Copyright © 2016年 abel jing. All rights reserved.
+//
+
+import UIKit
+
+class RollView: UIView {
+    @IBOutlet var upperView: UIView!
+    
+    @IBOutlet var belowView: UIView!
+}
