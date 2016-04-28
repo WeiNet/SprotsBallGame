01c68c1885cd374b64cbc4834a8e6f6bfa13b28d
Brook He <heyupei@brookmacbook-air.local>
2016-04-28 14:31:50 +0800

    反反复复

--- SportsBall/RollView.swift
+++ SportsBall/RollView.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class RollView: UIView {
+class RollView: UIView,GestureDelegate {
     @IBOutlet var upperView: UIView!
-    
     @IBOutlet var belowView: UIView!
+    
+    @IBOutlet var N_LRFBLView: UIView!
+    @IBOutlet var N_LRFBL: UILabel!
+    @IBOutlet var N_LRFPL: UILabel!
+    @IBOutlet var N_RRFBLView: UIView!
+    @IBOutlet var N_RRFBL: UILabel!
+    @IBOutlet var N_RRFPL: UILabel!
+    
+    @IBOutlet var N_LDXBLView: UIView!
+    @IBOutlet var N_LDXBL: UILabel!
+    @IBOutlet var N_LDXDPL: UILabel!
+    @IBOutlet var N_RDXBLView: UIView!
+    @IBOutlet var N_RDXBL: UILabel!
+    @IBOutlet var N_RDXXPL: UILabel!
+    
+    //启动用户交互事件，设定Tag用于区别点击时所在的控件
+    func userInteractionEnabled(){
+    }
+    
+    //注册点击事件
+    func addGestureRecognizer(){
+    }
 }
