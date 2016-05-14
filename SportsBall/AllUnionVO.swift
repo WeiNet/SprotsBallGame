52201c06dbecdcf465be6eded6a64c85906f3441
Brook He <heyupei@brookmacbook-air.local>
2016-05-14 09:07:22 +0800

    all

---
+++ SportsBall/AllUnionVO.swift
@@ -0,0 +1,21 @@
+//
+//  AllUnionVO.swift
+//  SportsBall
+//
+//  Created by Brook on 16/5/14.
+//  Copyright © 2016年 abel jing. All rights reserved.
+//
+
+import UIKit
+
+class AllUnionVO: NSObject {
+    var arrayUnionVO:Array<UnionTitleVO> = Array()
+    
+    private static let allUnionVOInstance = AllUnionVO()
+    
+    class func getAllUnionVOInstance() -> AllUnionVO {
+        return allUnionVOInstance
+    }
+    
+    private override init() {} // 私有化init方法
+}
