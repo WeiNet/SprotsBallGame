0e7f5eca8acd6164327f2633bbbf19c49bc4a12b
Brook He <heyupei@brookmacbook-air.local>
2016-05-18 13:48:45 +0800

    页面调整

--- SportsBall/BallViewController.swift
+++ SportsBall/BallViewController.swift
@@ -110,7 +110,7 @@ class BallViewController: UIViewController {
         
         var startY:CGFloat = 0
         let width = contentView.frame.size.width
-        var height = contentView.frame.size.height + 20
+        var height = contentView.frame.size.height
         
         if (playType != "2") {
             let cartButtonView = NSBundle.mainBundle().loadNibNamed("CartButtonView" , owner: nil, options: nil).first as? CartButtonView
@@ -145,7 +145,6 @@ class BallViewController: UIViewController {
         tableView.orderHeight = orderHeight
         tableView.bindDelegate = delegate
         contentView.addSubview(tableView)
-        var tttt:String = "ttt"
     }
     
     //绑定队伍标题
