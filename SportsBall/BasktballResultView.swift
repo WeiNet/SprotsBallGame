fd738040650bf8ed4da5e4a0a06fcc69efca9c49
Brook He <heyupei@brookmacbook-air.local>
2016-06-06 10:16:35 +0800

    class

---
+++ SportsBall/BasktballResultView.swift
@@ -0,0 +1,21 @@
+//
+//  BasktballResultView.swift
+//  SportsBall
+//
+//  Created by Brook on 16/6/6.
+//  Copyright © 2016年 abel jing. All rights reserved.
+//
+
+import UIKit
+
+class BasktballResultView: UIView {
+    @IBOutlet var N_VISIT_NAME: UILabel!
+    @IBOutlet var N_HOME_NAME: UILabel!
+    @IBOutlet var N_GAMEDATE: UILabel!
+    @IBOutlet var N_UP_VISIT_RESULT: UILabel!
+    @IBOutlet var N_UP_HOME_RESULT: UILabel!
+    @IBOutlet var N_DO_VISIT_RESULT: UILabel!
+    @IBOutlet var N_DO_HOME_RESULT: UILabel!
+    @IBOutlet var N_VISIT_RESULT: UILabel!
+    @IBOutlet var N_HOME_RESULT: UILabel!
+}
